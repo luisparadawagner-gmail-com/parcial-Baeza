3d81fbf191fc5f9bc27ccc7fb05c8c9cb6266f86
LUIS ALBERTO PARADA <luisparadawagner@gmail.com>
2020-07-05 01:26:18 -0300

    Correcciones

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,37 +4,35 @@ import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MatTableModule} from '@angular/material/table';
-import {MatMenuModule} from '@angular/material/menu';
+import { MatTableModule } from '@angular/material/table';
+import { MatMenuModule } from '@angular/material/menu';
 import { MatButtonModule } from '@angular/material/button';
 import { HttpClientModule } from '@angular/common/http';
 import { InscribirComponent } from './inscribir/inscribir.component';
 import { ConsultarComponent } from './consultar/consultar.component';
 import { ConectaService } from './servicios/conecta.service';
-import {MatFormFieldModule} from '@angular/material/form-field';
-
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import {MatSelectModule} from '@angular/material/select';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    InscribirComponent,
-    ConsultarComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule,
-    MatTableModule,
-    MatMenuModule, 
-    ReactiveFormsModule,
-    FormsModule,
-    MatButtonModule,
-    HttpClientModule,
-    MatFormFieldModule
+	declarations: [ AppComponent, InscribirComponent, ConsultarComponent ],
+	imports: [
+		BrowserModule,
+		AppRoutingModule,
+		BrowserAnimationsModule,
+		MatTableModule,
+		MatMenuModule,
+		ReactiveFormsModule,
+		FormsModule,
+		MatButtonModule,
+		HttpClientModule,
+		MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule
+	],
 
-  ],
-
-  providers: [ConectaService],
-  bootstrap: [AppComponent]
+	providers: [ ConectaService ],
+	bootstrap: [ AppComponent ]
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/inscribir/inscribir.component.ts
+++ src/app/inscribir/inscribir.component.ts
@@ -4,56 +4,48 @@ import { ActivatedRoute, Router } from '@angular/router';
 import { Alumno } from '../clases/alumno';
 
 @Component({
-  selector: 'app-inscribir',
-  templateUrl: './inscribir.component.html',
-  styleUrls: ['./inscribir.component.css']
+	selector: 'app-inscribir',
+	templateUrl: './inscribir.component.html',
+	styleUrls: [ './inscribir.component.css' ]
 })
 export class InscribirComponent implements OnInit {
+	constructor(private fb: FormBuilder, private route: ActivatedRoute, private router: Router) {}
+	// mate: Materia[] = [
+	//   {value: 'Logica', viewValue: 'Logica'},
+	//   {value: 'Marketing', viewValue: 'Marketing'},
+	//   {value: 'Proyectos', viewValue: 'Proyectos'}
+	// ];
 
-  constructor(private fb: FormBuilder,private route: ActivatedRoute, private router:Router) { }
-  // mate: Materia[] = [
-  //   {value: 'Logica', viewValue: 'Logica'},
-  //   {value: 'Marketing', viewValue: 'Marketing'},
-  //   {value: 'Proyectos', viewValue: 'Proyectos'}
-  // ];
+	inscripcionForm: FormGroup;
+	parametro: any;
+	alumno: Alumno;
+	ngOnInit(): void {
+		this.parametro = this.route.snapshot.params;
 
-inscripcionForm: FormGroup;
-parametro:any;
-alumno: Alumno;
-  ngOnInit(): void {
-    this.parametro= this.route.snapshot.params;
-  
+		if (Object.keys(this.parametro).length) {
+			this.alumno = this.parametro;
+		}
 
-  if (Object.keys(this.parametro).length) {
-    this.alumno = this.parametro;
-  }
+		this.initForm(this.alumno);
+	}
 
-  this.initForm(this.alumno);
-}
+	initForm(elemento: Alumno) {
+		this.inscripcionForm = this.fb.group({
+			nombre: [ elemento ? elemento.nombre : '', Validators.required ],
+			apellido: [ elemento ? elemento.apellido : '' ],
+			edad: [ elemento ? elemento.edad : '' ],
 
+			materia: [ elemento ? elemento.materia : '' ],
+			aniocursada: [ elemento ? elemento.aniocursada : '' ]
+		});
+	}
 
+	// submit(){
 
-initForm (elemento : Alumno){
+	// }
 
- this.inscripcionForm = this.fb.group({
- nombre : [elemento ? elemento.nombre:'',Validators.required],
- apellido : [elemento ? elemento.apellido:''],
- edad : [elemento? elemento.edad:''],
-
- materia : [elemento? elemento.materia:''],
- aniocursada : [elemento? elemento.aniocursada:''],
- }); 
-};
-
-// submit(){
- 
-// }
-
-
-cargarDatos(){
-let alum : Alumno = this.inscripcionForm.value;
-this.router.navigate(['consultar-component', alum]);
-
-};
- 
+	cargarDatos() {
+		let alum: Alumno = this.inscripcionForm.value;
+		this.router.navigate([ 'consultar-component', alum ]);
+	}
 }
